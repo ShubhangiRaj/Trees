9b63e00f984550d5ad1256697fe21a756e58d364
Shubhangi Agrawal <shubhi.raj13@gmail.com>
2018-08-13 09:17:20 +0530

    new ques

--- BTrees/numOfLeaves.js
+++ BTrees/numOfLeaves.js
@@ -56,6 +56,7 @@ bTree.insertElem(bTree.root, 4);
 bTree.insertElem(bTree.root, 5);
 bTree.insertElem(bTree.root, 6);
 bTree.insertElem(bTree.root, 7);
+bTree.insertElem(bTree.root, 8);
 
 function numOfLeaves(root){
     if(root == null){
@@ -79,3 +80,49 @@ function numOfLeaves(root){
     console.log(tempArr.length);
 }
 numOfLeaves(bTree.root);
+
+// find number of full nodes i.e. nodes with both children
+function fullNodes(root){
+    if(root == null){
+        return
+    }
+    let temp, count = 0, queue = [];
+    queue.push(root);
+    while(queue.length){
+        temp = queue.shift();
+        if(temp.left && temp.right){
+            count += 1;
+        }
+        if(temp.left !== null){
+            queue.push(temp.left);
+        }
+        if(temp.right !== null){
+            queue.push(temp.right);
+        }
+    }
+    console.log(count);
+}
+fullNodes(bTree.root);
+
+// find number of half nodes i.e. nodes with only one children
+function halfNodes(root){
+    if(root == null){
+        return
+    }
+    let temp, count = 0, queue = [];
+    queue.push(root);
+    while(queue.length){
+        temp = queue.shift();
+        if((temp.left && !temp.right) || (!temp.left && temp.right)){
+            count += 1;
+        }
+        if(temp.left !== null){
+            queue.push(temp.left);
+        }
+        if(temp.right !== null){
+            queue.push(temp.right);
+        }
+    }
+    console.log(count);
+}
+halfNodes(bTree.root);
